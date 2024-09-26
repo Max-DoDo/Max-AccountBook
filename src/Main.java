8824eaeb9230999e94ad87581b0803272f4092bc
Max <great_maxwell@outlook.com>
2024-09-26 19:42:11 +0100

    doc: CHANGELOG added
    see changelog for more details

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import ui.AccountBookFrame;
+
+public final class Main {
+
+    private Main(){}
+
+    public static void main(String[] args){
+        System.out.println("Hallo Word");
+        new AccountBookFrame();
+    }
+}
---
+++ src/ui/AccountBookFrame.java
@@ -0,0 +1,10 @@
+package ui;
+
+import javax.swing.*;
+
+public class AccountBookFrame extends JFrame {
+
+    public AccountBookFrame(){
+
+    }
+}
